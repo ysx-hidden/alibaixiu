6c41b11322e5578cd8cd26266e266c9e34e5e7b3
dx <dx@dx.com>
2020-03-21 15:06:40 +0800

    完成退出登录

---
+++ public/assets/js/common.js
@@ -0,0 +1,13 @@
+$("#logout").on("click", function() {
+    let isConfirm = confirm("确定要退出吗？")
+    $.ajax({
+        type: "post",
+        url: "/logout",
+        success() {
+            location.href = "login.html"
+        },
+        error() {
+            alert("退出失败")
+        },
+    })
+})
