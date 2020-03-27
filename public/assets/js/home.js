241294c121c6d37818c80df051e073cf7fb7f106
dx <dx@dx.com>
2020-03-27 15:12:44 +0800

    完成首页热门推荐和最新发布

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -3,7 +3,7 @@ $.ajax({
     type: "get",
     url: "/slides",
     success(response) {
-        console.log(response)
+        // console.log(response)
         let html = template("slidesTpl", { data: response })
         $("#slidesBox").html(html)
         // 轮播图代码放在这里，因为ajax是异步的，只有ajax请求成功才能执行这个代码
@@ -27,5 +27,25 @@ $.ajax({
                 swiper.next()
             }
         })
+    },
+})
+// 获取热门推荐数据
+$.ajax({
+    type: "get",
+    url: "/posts/recommend",
+    success(response) {
+        // console.log(response)
+        let html = template("recommendTpl", { data: response })
+        $("#recommend").html(html)
+    },
+})
+// 最新发布
+$.ajax({
+    type: "get",
+    url: "/posts/lasted",
+    success(response) {
+        // console.log(response)
+        let html = template("newTpl", { data: response })
+        $("#new").append(html)
     },
 })
